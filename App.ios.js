d21ea1fb58ec95f07f666d413b446e5114857221
Anish Duwal <duwalanise@gmail.com>
2017-10-12 17:57:52 +0545

    initialize todo app

---
+++ App.ios.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from 'react';
+import Todo from './src/app/Todo';
+
+export default () => <Todo />;
